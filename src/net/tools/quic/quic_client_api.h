120d1b9d9bc82bf3712e98bdbfa0c8010f613d82
Ali Clark <ali@clark.gb.net>
2016-06-10 22:46:45 +0100

    add some api files to build

---
+++ src/net/tools/quic/quic_client_api.h
@@ -0,0 +1,15 @@
+
+#ifndef QUIC_CLIENT_API_H
+#define QUIC_CLIENT_API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int quic_client_main(int argc, char* argv[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* QUIC_CLIENT_API_H */
---
+++ src/net/tools/quic/quic_server_api.h
@@ -0,0 +1,15 @@
+
+#ifndef QUIC_SERVER_API_H
+#define QUIC_SERVER_API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int quic_server_main(int argc, char* argv[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* QUIC_SERVER_API_H */
